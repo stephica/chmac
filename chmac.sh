b1d652d1968f1a6f16125278cf2f4b113ed8c607
Omid <omidlink@gmail.com>
2015-01-31 22:50:09 -0500

    extend APname file not found msg

--- chmac.sh
+++ chmac.sh
@@ -208,7 +208,7 @@ if [[ ! -f "$netmansysconfile" ]]; then
     #
     # https://www.gnu.org/software/bash/manual/bashref.html
     #
-    PS3="There is no file named $APname under $netmansyscondir. Either you haven't connected to this Access Point before, or NetworkManager has saved it under a different name. Enter # of the file associated with $APname: "
+    PS3="There is no file named $APname under $netmansyscondir. Either you haven't connected to this Access Point before, or NetworkManager has saved it under a different name. ENTER # of the file associated with $APname, or ENTER n if this is the first time you are connecting to $APname or if you don't know which file is associated with $APname: "
     select netmansysconfile in $netmansyscondir/*
     do
 	break		# break after one file number is input
